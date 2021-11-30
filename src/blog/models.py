d5d693da6eed8c83d738166fe5300caec793aca8
SergiioDev <sergi13456@hotmail.es>
2021-11-30 13:36:46 +0100

    change slug field and add the user serializer in blog_api

--- src/blog/models.py
+++ src/blog/models.py
@@ -25,7 +25,7 @@ class Post(models.Model):
     title = models.CharField(max_length=250)
     excerpt = models.TextField(null=True)
     content = models.TextField()
-    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    slug = models.SlugField(max_length=250, unique_for_date='published')
     published = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='blog_posts')
     status = models.CharField(max_length=10, choices=options, default='published')
