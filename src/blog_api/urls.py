23d067061d87e2f3ed5f35c94ff871ff2c4feb9f
SergiioDev <sergi13456@hotmail.es>
2021-12-01 12:49:50 +0100

    create crud

--- src/blog_api/urls.py
+++ src/blog_api/urls.py
@@ -1,8 +1,19 @@
-from src.blog_api.views import PostList
+from django.urls import path
+
+from src.blog_api.views import PostList, PostDetail, CreatePost, AdminPostDetail, DeletePost, EditPost
 from rest_framework.routers import DefaultRouter
 
 app_name = 'src.blog_api'
 
-router = DefaultRouter()
-router.register('', PostList, basename='post')
-urlpatterns = router.urls
+# router = DefaultRouter()
+# router.register('', PostList, basename='post')
+# urlpatterns = router.urls
+
+urlpatterns = [
+    path('', PostList.as_view(), name='listpost'),
+    path('post/<str:pk>/', PostDetail.as_view(), name='detailpost'),
+    path('admin/create/', CreatePost.as_view(), name='createpost'),
+    path('admin/edit/postdetail/<int:pk>/', AdminPostDetail.as_view(), name='admindetailpost'),
+    path('admin/edit/<int:pk>/', EditPost.as_view(), name='editpost'),
+    path('admin/delete/<int:pk>/', DeletePost.as_view(), name='deletepost'),
+]
--- src/blog_api/views.py
+++ src/blog_api/views.py
@@ -7,27 +7,63 @@ from .serializers import PostSerializer
 from rest_framework.permissions import BasePermission, SAFE_METHODS, IsAuthenticated
 
 
-class PostUserWritePermission(BasePermission):
-    message = "Editing posts is restricted to the author only."
+# class PostUserWritePermission(BasePermission):
+#   message = "Editing posts is restricted to the author only."
 
-    def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS:
-            return True
+#  def has_object_permission(self, request, view, obj):
+#     if request.method in SAFE_METHODS:
+#        return True
 
-        return obj.author == request.user
+#   return obj.author == request.user
 
-
-class PostList(viewsets.ViewSet):
-    #permission_classes = [IsAuthenticated]
+class PostList(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = PostSerializer
     queryset = Post.post_objects.all()
 
-    def list(self, request):
-        serializer = PostSerializer(self.queryset, many=True)
-        return Response(serializer.data)
 
-    def retrieve(self, request, pk=None):
-        post = get_object_or_404(self.queryset, pk=pk)
-        serializer = PostSerializer(post)
-        return Response(serializer.data)
+class PostDetail(generics.RetrieveAPIView):
+    serializer_class = PostSerializer
+
+    def get_object(self, queryset=None, **kwargs):
+        item = self.kwargs.get('pk')
+        return get_object_or_404(Post, slug=item)
 
 
+class CreatePost(generics.CreateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Post.post_objects.all()
+    serializer_class = PostSerializer
+
+
+class AdminPostDetail(generics.RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Post.post_objects.all()
+    serializer_class = PostSerializer
+
+
+class EditPost(generics.UpdateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Post.post_objects.all()
+    serializer_class = PostSerializer
+
+
+class DeletePost(generics.RetrieveDestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Post.post_objects.all()
+    serializer_class = PostSerializer
+
+
+# Easy form
+# class PostList(viewsets.ViewSet):
+#  permission_classes = [IsAuthenticated]
+#  queryset = Post.post_objects.all()
+
+# def list(self, request):
+#    serializer = PostSerializer(self.queryset, many=True)
+#   return Response(serializer.data)
+
+# def retrieve(self, request, pk=None):
+#    post = get_object_or_404(self.queryset, pk=pk)
+#   serializer = PostSerializer(post)
+#   return Response(serializer.data)
